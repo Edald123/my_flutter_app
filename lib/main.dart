dc5f088ec463bad9cb51d59f79d5efac3d2eb9fb
Edald123 <eduardoahumada@EDCMXENLT0646.local>
2021-11-30 20:29:23 -0600

    Added Column and ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,41 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override // decorator provided by Dart, make code clearer
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My First App'),
         ),
-        body: const Text('This is my default text!'),
+        body: Column(
+          children: [
+            const Text('The question!'),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Answer 2 chosen!'),
+              child: const Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // ...
+                print('Answer 3 chosen!');
+              },
+              child: const Text('Answer 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
